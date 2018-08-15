9ce2566d6c22c86b08c025d31d7833fb25d57eb8
ma539 <ma539@cornell.edu>
2018-08-15 10:39:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    # binding.pry
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
